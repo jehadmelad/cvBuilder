2a827a4f17fa02070b17b915049dacaa0eaaad4b
jehadmelad <jd.melad@gmail.com>
2022-11-30 13:33:36 +0100

    adding js file

---
+++ builder/static/js/main.js
@@ -0,0 +1,30 @@
+function generatePDF() {
+    // Choose the element that our invoice is rendered in.
+    const element = document.getElementById('resumeTempl');
+
+
+    var opt = {
+            // margin:       [0.1, 0.1,0.1,0.1],
+            filename:     'myfile.pdf',
+            // image:        { type: 'jpeg', quality: 0.99 },
+            html2canvas:  { scale:5 },
+            // jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' },
+            // jsPDF:        { unit: 'in', format: 'letter', orientation: 'l' },
+
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' },
+// 
+            pagebreak:     {mode: ['avoid-all','css','legacy']},
+
+            // pagebreak:     {mode: ['avoid-all','css']},
+            // pagebreak:     {mode: ['avoid-all']},
+
+            // pagebreak:     {mode: ['css']},
+            // pagebreak:     {mode: ['legacy']}
+
+
+            };
+
+                    // Choose the element and save the PDF for our user.
+    // New Promise-based usage:
+    html2pdf().set(opt).from(element).save();
+}
